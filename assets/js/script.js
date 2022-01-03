80346e06840ed66bab1197f8696dcc4f72fbb07b
Rhea <rheam97@gmail.com>
2022-01-03 17:32:50 -0500

    pseudocode js, html hidden and visible elements, potentially different html for saved playlists

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-//test
+//user inputs category (general could be default? dropdown), source (typed in: grab value and trim), 
+//language (dropdown)
+    // atleast one input otherwise error message
+//fetches api using appropriate parameters
+    //foundation: find display none and put that for homepage, 
+    //and then we change that attriobute once the input is entered
+//modal + calendar: sort dropdown with popularity and/or publish date
+    //hidden on homepage, shows up after search in the newstitle element
+    // user clicks on sort and pulls up modal
+    //fetches api with sort
+// pulls up articles within specified parameters 
+
+//when they click on the article the speaker starts reading the text
+    //use modal to change aspects of speaker
+    //they can change the voice, pitch, rate, and volume of the speaker
+    
+//they have the option to save the search inputs into a "playlist"
+    // pull up new modal from save button 
+    // saves to local storage
+    // button on the homepage to get the item from localstorage (if not  null)
+    // send the user to a new htmL?
+    // get item from local storage to persist on homepage?
+        //can this playlist update automatically with most recent posts from sources/category
+        // set attribute to display visible
