0f0885711271ea25850a8cca6db9e6edf4a829c2
Sung Yoo <yoowook1207@gmail.com>
2022-01-03 19:28:56 -0500

    merged

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//user inputs category (general could be default? dropdown), source (typed in: grab value and trim), 
+//language (dropdown)
+    // at least one input otherwise error message
+//fetches api using appropriate parameters
+    //foundation: find display none and put that for homepage, 
+    //and then we change that attribute once the input is entered
+//modal + calendar: sort dropdown with popularity and/or publish date
+    //hidden on homepage, shows up after search in the news title element
+    // user clicks on sort and pulls up modal
+    //fetches api with sort
+// pulls up articles within specified parameters 
+
+//when they click on the article the speaker starts reading the text
+    //use modal to change aspects of speaker
+    //they can change the voice, pitch, rate, and volume of the speaker
+    
+//they have the option to save the search inputs into a "playlist"
+    // pull up new modal from save button 
+    // saves to local storage
+    // button on the homepage to get the item from local storage (if not  null)
+    // send the user to a new htmL?
+    // get item from local storage to persist on homepage?
+        //can this playlist update automatically with most recent posts from sources/category
+        // set attribute to display visible
