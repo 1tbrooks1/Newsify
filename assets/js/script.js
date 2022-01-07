7464ba68a3043302b10fcec53efd2e6ddb822b51
Rhea <rheam97@gmail.com>
2022-01-07 11:01:10 -0500

    deleted extra branch before big merge, edited classes in Dom references, altered syntaax, added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,111 +1,121 @@
 // DOM variables
-var sourceEl = document.querySelector("");
-var categoryEL = document.querySelector("");
-var languageEl = document.querySelector("");
 var articleContainer = document.querySelector(".article-section");
 
 function sourceInput() {
-  let outletChoice = document.querySelector(".source");
-  let outlet = outletChoice.value().trim();
-  console.log(outlet);
-  buildUrl(outlet);
+    let outletChoice = document.querySelector("#press-names");
+    let outlet = outletChoice.value.trim();
+    console.log(outlet);
+    buildUrl(outlet);
 }
 
 function categoryDd() {
-  let categoryChoice = document.querySelector(".category");
-  let category = categoryChoice.value;
-  console.log(category);
-  buildUrl(category);
+    let categoryChoice = document.querySelector(".dropdown-category");
+    let category = categoryChoice.value;
+    console.log(category);
+    buildUrl(category);
 }
 
 function languageDd() {
-  let languageChoice = document.querySelector(".language");
-  let language = languageChoice.value;
-  console.log(language);
-  buildUrl(language);
+    let languageChoice = document.querySelector(".dropdown-language");
+    let language = languageChoice.value;
+    console.log(language);
+    buildUrl(language);
 }
 
+// needs to be dropdown in html, wont work possibly like this
 function sortList() {
-  let sortChoice = document.querySelector(".sort");
-  let sort = sortChoice.value;
-  console.log(sort);
-  buildUrl(sort);
-  displayArticles();
+    let sortChoice = document.querySelector(".control-panel");
+    let sort = sortChoice.value;
+    console.log(sort);
+    buildUrl(sort);
+    displayArticles();
 }
 
 function getDate() {
-  let from = document.querySelector(".from");
-  let until = document.querySelector(".until");
-  let date1 = from.value();
-  let date2 = until.value();
-  buildUrl(date1, date2);
+    let from = document.querySelector(".from");
+    let until = document.querySelector(".until");
+    let date1 = from.value;
+    let date2 = until.value;
+    buildUrl(date1, date2);
 }
 
 function buildUrl(outlet, category, language, sort, date1, date2) {
-  const apiKey = "afe8ca7e3a00ff67fd299fec29cce5c7";
-  let apiUrl = `http://api.mediastack.com/v1/news?sources=${outlet}&categories=${category}&languages=${language}&sort=${sort}&date=${date1},${date2}&access_key=${apiKey}`;
+    const apiKey = "afe8ca7e3a00ff67fd299fec29cce5c7";
+    let apiUrl = `http://api.mediastack.com/v1/news?sources=${outlet}&categories=${category}&languages=${language}&sort=${sort}&date=${date1},${date2}&access_key=${apiKey}`;
 
-  if (outlet || category || language) {
-    fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            console.log(data);
-            displayArticles(data);
-            //local storage function call
-          });
-        } else {
-          // response failed
-          let noArticleError = document.createElement("modal");
-          noArticleError.textContent = "Error: No articles found.";
-          noArticleError.setAttribute("class", "reveal");
-          // can you set attribute with setattribute??
-          noArticleError.setAttribute("attribute", "data-open");
-          let vanish = document.createElement("button");
-          vanish.textContent = "Close";
-          noArticleError.appendChild(vanish);
-          vanish.setAttribute("attribute", "data-close");
-        }
-      })
-      .catch(function (error) {
-        // modal: error 404: bad network connection.
-      });
-  } else if (!outlet && !category && !language) {
-    // modal: You must input atleast one of the parameters in the search box.
-  }
+    if (outlet || category || language) {
+        fetch(apiUrl)
+            .then(function (response) {
+                if (response.ok) {
+                    response.json().then(function (data) {
+                        console.log(data);
+                        displayArticles(data);
+                        //local storage function call
+                    });
+                } else {
+                    // response failed
+                    let error1 = document.querySelector("#no-articles")
+                    error1.setAttribute("class", "reveal")
+                    // not sure if blank quotes work to set attribute instead of class
+                    error1.setAttribute("", "data-reveal")
+                    let error1Btn = document.querySelector("#no-articles-btn")
+                    error1Btn.setAttribute("", "data-close")
+                }
+            })
+            .catch(function (error) {
+                // modal: error 404: bad network connection.
+                // let error1 = document.querySelector("#no-articles")
+                //     error1.setAttribute("class", "reveal")
+                //     // not sure if blank quotes work to set attribute instead of class
+                //     error1.setAttribute("", "data-reveal")
+                //     let error1Btn = document.querySelector("#no-articles-btn")
+                //     error1Btn.setAttribute("", "data-close")
+            });
+    } else if (!outlet && !category && !language) {
+        // modal: You must input atleast one of the parameters in the search box.
+        // let error1 = document.querySelector("#no-articles")
+        //             error1.setAttribute("class", "reveal")
+        //             // not sure if blank quotes work to set attribute instead of class
+        //             error1.setAttribute("", "data-reveal")
+        //             let error1Btn = document.querySelector("#no-articles-btn")
+        //             error1Btn.setAttribute("", "data-close")
+    }
 }
 
 function displayArticles(data) {
-  for (i = 0; i < data.length; i++) {
-    let articleEl = document.createElement("div");
-    articleContainer.appendChild(articleEl);
+    for (i = 0; i < data.length; i++) {
+        let articleEl = document.createElement("div");
+        articleContainer.appendChild(articleEl);
 
-    let articleTitle = document.createElement("h4");
-    articleTitle.textContent = data.data[i].title;
-    articleEl.appendChild(articleTitle);
+        let articleTitle = document.createElement("h4");
+        articleTitle.textContent = data.data[i].title;
+        articleEl.appendChild(articleTitle);
 
-    let articleDescription = document.createElement("p");
-    articleDescription.textContent = data.data[i].description;
-    articleTitle.appendChild(articleDescription);
+        let articleDescription = document.createElement("p");
+        articleDescription.textContent = data.data[i].description;
+        articleTitle.appendChild(articleDescription);
 
-    let articleUrl = document.createElement("a");
-    articleUrl.text = data.data[i].url;
-    articleTitle.appendChild(articleUrl);
+        let articleUrl = document.createElement("a");
+        articleUrl.text = data.data[i].url;
+        articleTitle.appendChild(articleUrl);
 
-    let articleSource = document.createElement("p");
-    articleSource.textContent = data.data[i].source;
-    articleTitle.appendChild(articleSource);
+        let articleSource = document.createElement("p");
+        articleSource.textContent = data.data[i].source;
+        articleTitle.appendChild(articleSource);
 
-    let articleImage = document.createElement("img");
-    articleImage.setAttribute("src", data.data[i].image);
-    articleTitle.appendChild(articleImage);
+        let articleImage = document.createElement("img");
+        articleImage.setAttribute("src", data.data[i].image);
+        articleTitle.appendChild(articleImage);
 
-    readInformation(articleTitle, articleDescription, articleSource);
-  }
+        // speaker button dynamically generates
+        // event listener for read information
+
+        readInformation(articleTitle, articleDescription, articleSource);
+    }
 }
 
 function readInformation(title, description, source) {
-    
+
 
 }
 
