aef950486dedfb7bafb2f1898c427dda1d6a4cee
Rhea <rheam97@gmail.com>
2022-01-11 13:47:45 -0500

    removed console logs, added descriptive comments, removed unnecessary variables, tried to fix null image append

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,115 +1,75 @@
-//user inputs category (general could be default? dropdown), source (typed in: grab value and trim), 
-//language (dropdown)
-// at least one input otherwise error message modal
-// this dynamically generates a search history input button on the page
-//fetches api using appropriate parameters
-//foundation: find display none and put that for homepage, 
-//and then we change that attribute once the input is entered
 
 // DOM variables
 let articleContainer = document.querySelector(".article-section");
 let searchBtn = document.getElementById("search-button");
 let sortChoice = document.querySelector(".dropdown-sort");
+let outletChoice = document.querySelector("#press-names");
+let categoryChoice = document.querySelector(".dropdown-category");
+let languageChoice = document.querySelector(".dropdown-language");
 let clearBtn = document.querySelector(".clear-button")
-let keywordEL = document.querySelector(".keyword")
-let excludedEl = document.querySelector(".exclude")
-// speaker html elements for voice, volume, pitch rate 
-let speakerVoiceEl = document.querySelector("dropdown")
-let volumeEl = document.querySelector(".speaker-volume");
-let pitchEl = document.querySelector(".pitch-rate");
-let rateEl = document.querySelector(".rate");
+let keywordEL = document.querySelector("#keywords")
+let excludedEl = document.querySelector("#excluding")
 
 
 // global variables
 let history = JSON.parse(localStorage.getItem("inputs")) || []
-//add keyword and exlcuded
-let userInputs = { source: "", topic: "", text: "", order: "" }
 
-// tied to an event listener at the bottom, operational
-// function sortList(outlet, category, language) {
-//     let sort = sortChoice.value;
-//     console.log(sort);
-//     buildUrl(outlet, category, language, sort)
-// }
+let userInputs = { source: "", topic: "", text: "", order: "", word: "", nothanks: "" }
 
-
-// // operational
-// function setVolumes() {
-//     speakerVolume = volumeEl.value;
-//     console.log("volume: " + speakerVolume);
-//     pitchRate = pitchEl.value;
-//     console.log("pitch: " + pitchRate);
-//     speedRate = rateEl.value;
-//     console.log("rate: " + speedRate);
-
-//     speakerSettings(speakerVolume, pitchRate, speedRate);
-// }
-
-// // not working at the moment
-// function speakerSettings(sVolume, sPitch, sRate) {
-//     console.log(sVolume, sPitch, sRate);
-//     responsiveVoice.speak({ volume: sVolume }, { pitch: sPitch }, { rate: sRate });
-//     console.log(volume.value, pitch.value, rate.value);
-// }
 
 function formSubmitHandler(event) {
+    // to prevent page refresh on click
     event.preventDefault()
-    // get values of dropdowns
-    let outletChoice = document.querySelector("#press-names");
+    // get values of dropdowns and inputs
     let outlet = outletChoice.value.trim();
-    let categoryChoice = document.querySelector(".dropdown-category");
     let category = categoryChoice.value;
-    let languageChoice = document.querySelector(".dropdown-language");
     let language = languageChoice.value;
     let sort = sortChoice.value
-    // let keyword = keywordEL.value.trim()
-    // let excluded= excludedEl.value.trim()
+    let keyword = keywordEL.value.trim()
+    let excluded = excludedEl.value.trim()
 
-    if (outlet || category || language || sort) // add keyword and excluded 
+    if (outlet || category || language || sort || keyword || excluded) // if any are entered
     {
         // if input combinations are unique, set to localstorage and show in history DOM
-        if (userInputs.source !== outlet ||
-            userInputs.topic !== category ||
-            userInputs.text !== language ||
-            userInputs.order !== sort
-             ) {
+        if (userInputs.source !== outlet || userInputs.topic !== category ||
+            userInputs.text !== language || userInputs.order !== sort ||
+            userInputs.word !== keyword || userInputs.nothanks !== excluded) {
 
             userInputs = {
                 source: outlet,
                 topic: category,
                 text: language,
-                order: sort
+                order: sort,
+                word: keyword,
+                nothanks: excluded
             }
+
             history.push(userInputs)
             localStorage.setItem("inputs", JSON.stringify(history))
             showHistory(history)
-            //sortList(outlet, category, language)
+            // fetch api with inputs
+            buildUrl(outlet, category, language, sort, keyword, excluded)
 
-            //add keyword and exlcuded
-            buildUrl(outlet, category, language, sort)
-
-
-        } // otherwise, show history anyways
+        } // otherwise, show old history anyways and fetch anyways
         else {
             showHistory(history)
+            buildUrl(outlet, category, language, sort, keyword, excluded)
 
         }
     }
 
 }
 
-//add keyword and exlcuded
-function buildUrl(outlet, category, language, sort) {
+function buildUrl(outlet, category, language, sort, keyword, excluded) {
     const apiKey = "50db9deb9962e4e61015af3116dce036";
-    // its saying sort comes up undefined ** add keyword and excluded
-    let apiUrl = `http://api.mediastack.com/v1/news?sources=${outlet}&categories=${category}&languages=${language}&sort=${sort}&limit=5&access_key=${apiKey}`;
+
+    let apiUrl = `http://api.mediastack.com/v1/news?sources=${outlet}&categories=${category}&languages=${language}&sort=${sort}&keywords=${keyword} -${excluded}&limit=5&access_key=${apiKey}`;
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(response);
-                    console.log(data);
+                    // display articles from data array on page
                     displayArticles(data);
 
                     if (data.data.length === 0) {
@@ -123,7 +83,7 @@ function buildUrl(outlet, category, language, sort) {
             }
         })
         .catch(function (error) {
-            // modal: error 404: bad network connection.
+            // error 404: bad network connection.
             $("#network-error").foundation("open")
         });
 }
@@ -131,91 +91,76 @@ function buildUrl(outlet, category, language, sort) {
 function displayArticles(data) {
     // to refresh the display with every search
     articleContainer.innerHTML = ""
-    for (i = 0; i < data.data.length; i++) {
-        // let next = 
-        // let sVolume = volumeEl.value;
-        // let sPitch = pitchEl.value;
-        // let sRate = rateEl.value;
 
-        // tried to grab object from api to change values dynamically but doesnt work properly
-        // SpeechSynthesisUtterance.voiceURI = // voice dropdwon value
-        // SpeechSynthesisUtterance.volume = sVolume
-        // SpeechSynthesisUtterance.pitch = sPitch
-        // SpeechSynthesisUtterance.rate = sRate
+    for (i = 0; i < data.data.length; i++) {
+        // dynamically create elements and assign textcontent to properties from data array objects
 
         let articleEl = document.createElement("div");
-        // ******margin doesnt work
-        articleEl.setAttribute("class", "border margin-bottom-2")
+
+        articleEl.setAttribute("class", "border div")
         articleContainer.appendChild(articleEl);
 
         let articleTitle = document.createElement("h3");
+
         let articleStop = document.createElement("button");
         let articleStart = document.createElement("button");
-        // let nextBtn = document.createElement("button")
+
         articleStart.textContent = "Start Article"
         articleStop.textContent = "Stop Article"
-        // nextBtn.textContent = "Skip"
 
-        // ****margin doesnt work here either 
-        articleStart.setAttribute("class", "button radius bordered shadow margin-horizontal-2");
-        articleStop.setAttribute("class", "button radius bordered shadow margin-horizontal-2");
-        nextBtn.setAttribute("class", "button radius bordered shadow margin-horizontal-2 primary");
+
+        articleStart.setAttribute("class", "button radius bordered shadow img");
+        articleStop.setAttribute("class", "button radius bordered shadow img");
+
 
         articleTitle.textContent = data.data[i].title;
+        responsiveVoice.speak(`${data.data[i].title}`);
+        articleTitle.setAttribute("class", "title")
         articleEl.appendChild(articleTitle);
 
-        //**** grab speaker html values and plug them into responseVoice.speak , not working dynamically*/
-        responsiveVoice.speak(`${data.data[i].title}`);
-
+        // start and stop speaking the articles
         articleStart.addEventListener("click", function () {
             responsiveVoice.speak(`${articleTitle.textContent} 
             ${articleDescription.textContent} ${articleSource.textContent}`)
-            console.log(i) // logging 5????
         })
         articleStop.addEventListener("click", function () {
-            responsiveVoice.pause();
+            responsiveVoice.cancel();
         })
-        // *****next btn doesnt skip
-        // nextBtn.addEventListener("click", function (event) {
-        //     // for (i=i+0; i < data.data.length; i++) {
-        //         if (event) {
-        //             console.log(event)
-        //         }
 
-        //     // }
-        //     // responsiveVoice.speak(`${data.data[i].title} 
-        //     // Article description is ${data.data[i].description}
-        //     // The source of the article comes from ${data.data[i].source}`)
-
-        // })
 
         let articleDescription = document.createElement("p");
         articleDescription.textContent = data.data[i].description;
+        articleDescription.setAttribute("class", "text")
 
         responsiveVoice.speak(`Article description is ${data.data[i].description}`);
         articleEl.appendChild(articleDescription);
 
         let articleUrl = document.createElement("a");
         articleUrl.textContent = `URL: ${data.data[i].url}`;
+        articleUrl.setAttribute("class", "text")
         articleEl.appendChild(articleUrl);
 
         let articleSource = document.createElement("p");
         articleSource.textContent = `Source: ${data.data[i].source}`;
+        articleSource.setAttribute("class", "text")
         responsiveVoice.speak("The source of the article comes from");
         responsiveVoice.speak(data.data[i].source);
         articleEl.appendChild(articleSource);
 
         let articleImage = document.createElement("img");
         articleImage.setAttribute("src", data.data[i].image);
+        articleImage.setAttribute("class", "img")
+
         // to prevent page clutter, remove null images
-        if (data.data[i].image !== null) {
+        if (data.data[i].image !== null || data.data[i].image !== ""
+            || data.data[i].image !== `${null}`
+            || data.data[i].image !== undefined) {
             articleEl.appendChild(articleImage)
         }
         // to make button elements display block
         let buttonDivEl = document.createElement("div")
         buttonDivEl.appendChild(articleStart)
         buttonDivEl.appendChild(articleStop)
-        buttonDivEl.appendChild(nextBtn)
         articleEl.appendChild(buttonDivEl)
     }
 }
@@ -224,23 +169,25 @@ function showHistory(history) {
     let searchHistoryEl = document.getElementById("search-history")
     // to allow it to clear when clear button is pressed
     searchHistoryEl.innerHTML = ""
+
     for (let i = 0; i < history.length; i++) {
         let searchedInputEl = document.createElement("button")
         searchHistoryEl.appendChild(searchedInputEl)
         searchHistoryEl.setAttribute("style", "display:flex-column")
         searchedInputEl.classList.add("history-button")
-        // ***add keyword and excluded properties
-        searchedInputEl.textContent = `${i + 1}.${history[i].source} ${history[i].topic} ${history[i].text}`
+        // display content of old searches to distinguish between them along with index of search
+        searchedInputEl.textContent =
+            `${i + 1}.${history[i].source} ${history[i].topic} ${history[i].text} ${history[i].word} ${history[i].nothanks}`
         searchedInputEl.style.display = "block"
         // fetch the api with old searches
         searchedInputEl.addEventListener("click", function () {
-            // ***add keyword and excluded properties
-            buildUrl(history[i].source, history[i].topic, history[i].text, history[i].order)
+            buildUrl(history[i].source, history[i].topic, history[i].text, history[i].order, history[i].word, history[i].nothanks)
         })
     }
 }
 
 function clearHistory() {
+    // clear localstorage
     localStorage.clear()
 
     history = []
@@ -249,60 +196,11 @@ function clearHistory() {
 }
 
 
-
-// Event Listeners
-// Starts app
+// submits inputs to fetch api data 
 searchBtn.addEventListener("click", formSubmitHandler);
-
-// Sets volume settings
-// volumeEl.addEventListener("click", setVolumes);
-// pitchEl.addEventListener("click", setVolumes);
-// rateEl.addEventListener("click", setVolumes);
-
-
 // clears local storage and search history DOM
 clearBtn.addEventListener("click", clearHistory)
 // allow search history to persist on refresh
 showHistory(history)
 
 
-
-
-
-
-
-//user inputs category (general could be default? dropdown), source (typed in: grab value and trim),
-//language (dropdown)
-// at least one input otherwise error message modal
-//fetches api using appropriate parameters
-//foundation: find display none and put that for homepage,
-//and then we change that attribute once the input is entered
-//modal + calendar: sort dropdown with popularity and/or publish date
-//hidden on homepage, shows up after search in the news title element
-// user clicks on sort and pulls up modal
-//fetches api with sort
-// pulls up articles within specified parameters
-
-//when they click on the article the speaker starts reading the text
-    //use modal to change aspects of speaker
-    //they can change the voice, pitch, rate, and volume of the speaker
-    // when they save the speaker charcateristics, this also dynamically saves
-    //to homepage
-
-
-
-
-
-
-//wish list
-//use modal to change aspects of speaker
-//they can change the voice, pitch, rate, and volume of the speaker
-
-//they have the option to save the search inputs into a "playlist"
-// pull up new modal from save button
-// saves to local storage
-// button on the homepage to get the item from local storage (if not  null)
-// send the user to a new htmL?
-// get item from local storage to persist on homepage?
-//can this playlist update automatically with most recent posts from sources/category
-// set attribute to display visible
